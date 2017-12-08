c9b5d09895b59461b09655d910a65e3b089e744b
jmarshall <jmarshall@codero.com>
2017-12-08 15:08:21 -0600

    Finishes adding sample test

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -26,3 +26,9 @@ def test_total_3():
     cart = Cart(product_data)
     cart.add("CF1", 2)
     assert cart.total() == 11.23
+
+def test_total_4():
+    cart = Cart(product_data)
+    cart.add("AP1", 3)
+    cart.add("CH1", 1)
+    assert cart.total() == 16.61
