c21ad59cee1345652e9f415a4c1030bbbf20a325
jmarshall <jmarshall@codero.com>
2017-12-09 16:23:59 -0600

    Add volume to docker run command to track cart state via pickle

--- cmd.py
+++ cmd.py
@@ -9,16 +9,17 @@ from src.Cart import Cart
 from src.util import print_menu
 
 @click.command()
-def run():
+@click.option('--cart_file', default='cart.p', help='Path to saved cart (pickle file)')
+def run(cart_file):
     # cart = Cart()
     # cart.add("CH1")
     # cart.add("AP1")
     # cart.add("CF1")
     # cart.add('MK1')
     # pickle.dump( cart, open("cart.p", "wb" ))
-    cart = pickle.load(open("cart.p", "rb" ))
+    cart = pickle.load(open(cart_file, "rb" ))
     cart.add('MK1')
-    pickle.dump( cart, open("cart.p", "wb" ))
+    pickle.dump( cart, open(cart_file, "wb" ))
     click.echo(cart.print())
 
 if __name__ == '__main__':
