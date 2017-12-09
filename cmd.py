354f33d9d9b13a119d9da1da6400b6bd8706cae8
jmarshall <jmarshall@codero.com>
2017-12-09 14:25:51 -0600

    Implement saving cart via pickle

--- cmd.py
+++ cmd.py
@@ -1,4 +1,5 @@
 # System
+import pickle
 
 # Third-party
 import click
@@ -9,11 +10,15 @@ from src.util import print_menu
 
 @click.command()
 def run():
-    cart = Cart()
-    cart.add("CH1")
-    cart.add("AP1")
-    cart.add("CF1")
+    # cart = Cart()
+    # cart.add("CH1")
+    # cart.add("AP1")
+    # cart.add("CF1")
+    # cart.add('MK1')
+    # pickle.dump( cart, open("cart.p", "wb" ))
+    cart = pickle.load(open("cart.p", "rb" ))
     cart.add('MK1')
+    pickle.dump( cart, open("cart.p", "wb" ))
     click.echo(cart.print())
 
 if __name__ == '__main__':
